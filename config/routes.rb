bc7775c397dd871aa1bc40d731ef24ecbe2a1269
Frank <yat22011@gmail.com>
2022-05-17 00:30:22 +0800

    init ruby-on-rails blog tutorial

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  root "articles#index"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles do
+    resources :comments
+  end
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
